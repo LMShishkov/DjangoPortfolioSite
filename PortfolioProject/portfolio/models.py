eacf7f785679860eeb96e17421b38d087ce3ae07
Lubomir Shishkov <l.m.shishkov@gmail.com>
2020-08-04 17:42:26 +0300

    Creation of two models and admin user

--- PortfolioProject/portfolio/models.py
+++ PortfolioProject/portfolio/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from datetime import date
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='portfolio/images/')
     url = models.URLField(blank=True)
+    start_date = models.DateField(blank=True, default=date.today)
+    end_date = models.DateField(blank=True, default=date.today)
