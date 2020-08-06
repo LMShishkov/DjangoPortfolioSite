0db91a04e428996384ef4038e8e0b116d7eccf3c
Lubomir Shishkov <l.m.shishkov@gmail.com>
2020-08-06 11:28:02 +0300

    Add a new blog model, view and template

--- PortfolioProject/blog/models.py
+++ PortfolioProject/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField(default=date.today)
